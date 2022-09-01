780f83bc9568cd2fa44e848f979caea69b6952c7
Isaac Anyimiah <iandev096@gmail.com>
2022-09-01 14:12:04 +0000

    added authentication service boilerplate

---
+++ src/services/auth/index.ts
@@ -0,0 +1,13 @@
+export class AuthService {
+  private static instance: AuthService;
+
+  private constructor() {}
+
+  public static getInstance(): AuthService {
+    if (!AuthService.instance) {
+      AuthService.instance = new AuthService();
+    }
+
+    return AuthService.instance;
+  }
+}
