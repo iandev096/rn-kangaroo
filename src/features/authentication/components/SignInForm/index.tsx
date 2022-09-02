f10ac122339b516ac7209fb855aa3aaa6c72b6dc
Isaac Anyimiah <iandev096@gmail.com>
2022-09-02 16:07:05 +0000

    built SignInForm components

--- src/features/authentication/components/SignInForm/index.tsx
+++ src/features/authentication/components/SignInForm/index.tsx
@@ -1,9 +1,49 @@
 import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Button from "src/components/Button";
+import TextField from "src/components/TextField";
+import { FONT_FAMILY } from "src/constants/FONT";
 
 type Props = {};
 
 function SignInForm({}: Props) {
-  return <div>SignInForm</div>;
+  return (
+    <View>
+      <Text style={styles.title}>Sign in</Text>
+      <View style={styles.textFieldGroup}>
+        <View style={styles.textFieldContainer}>
+          <TextField
+            label="Email or Phone Number"
+            placeholder="eg. someone@email.com"
+            keyboardType="email-address"
+          />
+        </View>
+        <View style={styles.textFieldContainer}>
+          <TextField
+            label="Email or Phone Number"
+            placeholder="eg. someone@email.com"
+            keyboardType="email-address"
+          />
+        </View>
+      </View>
+      <Button title="Sign In" onPress={() => console.log("pressed")} />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  title: {
+    fontFamily: FONT_FAMILY.BR_FIRMA_MEDIUM,
+    fontSize: 48,
+    lineHeight: 58,
+  },
+  textFieldContainer: {
+    marginBottom: 20,
+  },
+  textFieldGroup: {
+    marginTop: 32,
+    marginBottom: 48,
+  },
+});
 
 export default SignInForm;
