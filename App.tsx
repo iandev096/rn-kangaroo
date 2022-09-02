90c8b60a58a02f8d6e772e3f4f41beddb464eb27
Isaac Anyimiah <iandev096@gmail.com>
2022-09-02 11:03:41 +0000

    setup splash screen and custom fonts

--- App.tsx
+++ App.tsx
@@ -1,6 +1,26 @@
 import "react-native-gesture-handler";
 import AppEntry from "src/features/navigation";
+import * as SplashScreen from "expo-splash-screen";
+import { useFonts } from "expo-font";
+import FONT from "src/constants/FONT";
+import { useEffect } from "react";
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded] = useFonts(FONT);
+
+  useEffect(
+    function handleFontsLoading() {
+      if (fontsLoaded) {
+        SplashScreen.hideAsync()
+          .then(() => console.log("FONTS LOADED"))
+          .catch((e) => console.warn(e));
+      }
+    },
+    [fontsLoaded]
+  );
+
   return <AppEntry />;
 }
