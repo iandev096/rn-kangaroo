427c25f79c3aceef517db26a0278a265b1b7b813
Isaac Anyimiah <iandev096@gmail.com>
2022-09-02 14:44:27 +0000

    refactor needed to work on authentication features

--- src/features/navigation/components/AuthStack/index.tsx
+++ src/features/navigation/components/AuthStack/index.tsx
@@ -1,15 +1,27 @@
-import { createStackNavigator } from "@react-navigation/stack";
+import {
+  createStackNavigator,
+  StackNavigationOptions,
+} from "@react-navigation/stack";
 import React from "react";
 import SignInScreen from "src/screens/SignInScreen";
 import SignUpScreen from "src/screens/SignUpScreen";
 
 const Stack = createStackNavigator();
+const defaultOptions: StackNavigationOptions = { headerShown: false };
 
 function AuthStack() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="SignUp" component={SignUpScreen} />
-      <Stack.Screen name="SignIn" component={SignInScreen} />
+      <Stack.Screen
+        options={defaultOptions}
+        name="SignIn"
+        component={SignInScreen}
+      />
+      <Stack.Screen
+        options={defaultOptions}
+        name="SignUp"
+        component={SignUpScreen}
+      />
     </Stack.Navigator>
   );
 }
