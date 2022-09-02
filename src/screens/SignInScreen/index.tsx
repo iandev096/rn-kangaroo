ade9f9bb268e8b92617321fdb1f9d508816e5f9f
Isaac Anyimiah <iandev096@gmail.com>
2022-09-02 16:09:57 +0000

    setup SignupScreen and used authentication feature - Authentication features     - AuthHeader     - SignInForm

--- src/screens/SignInScreen/index.tsx
+++ src/screens/SignInScreen/index.tsx
@@ -1,14 +1,46 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { FONT_FAMILY } from "src/constants/FONT";
+import { AuthHeader, SignInForm } from "src/features/authentication";
+import useScrollEnabled from "src/hooks/useScrollEnabled";
 
 type Props = {};
 
 function SignInScreen({}: Props) {
+  const { scrollEnabled, onLayout } = useScrollEnabled();
+
   return (
-    <View>
-      <Text>SignInScreen</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1 }} edges={["left", "right", "bottom"]}>
+      <ScrollView scrollEnabled={scrollEnabled}>
+        <View onLayout={onLayout}>
+          <AuthHeader />
+          <View style={styles.form}>
+            <SignInForm />
+          </View>
+          <View>
+            <Text style={styles.version}>V.1.0</Text>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  form: {
+    marginTop: 48,
+    paddingHorizontal: 16,
+  },
+  version: {
+    fontFamily: FONT_FAMILY.BR_FIRMA_REGULAR,
+    lineHeight: 24,
+    fontSize: 12,
+    textAlign: "center",
+    marginTop: 58,
+    color: "#6b6b6b",
+  },
+});
 
 export default SignInScreen;
