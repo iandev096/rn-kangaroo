636df7df3017c619f6949ebca07cba8053e8a0a1
Isaac Anyimiah <iandev096@gmail.com>
2022-09-02 17:11:49 +0000

    scroll enables when keyboard is visible

--- src/hooks/useScrollEnabled.ts
+++ src/hooks/useScrollEnabled.ts
@@ -1,20 +1,38 @@
-import { useState } from "react";
-import { LayoutChangeEvent, useWindowDimensions } from "react-native";
+import { useEffect, useState } from "react";
+import { Keyboard, LayoutChangeEvent, useWindowDimensions } from "react-native";
 
 /**
  * This hook is intended to be used with any scrollable view.
  * It compares the height of a view to the height of the current window and if the height
  * of the view is greater than the current window, ```scollEnabled``` is set to true; otherwise false.
+ * When Keyboard is open, we also set ```scrollEnabled``` to true; otherwise false
  * ```onLayout``` must be set on the view in question.
  */
 
 function useScrollEnabled() {
-  const [scrollEnabled, setScrollEnabled] = useState(false);
+  const [keyboardShown, setKeyboardShown] = useState(false);
+  const [viewOverflows, setviewOverflows] = useState(false);
+
+  const scrollEnabled = viewOverflows || keyboardShown;
+
+  useEffect(function handleKeyboardVisibility() {
+    const showSubscription = Keyboard.addListener("keyboardDidShow", () =>
+      setKeyboardShown(true)
+    );
+    const hideSubscription = Keyboard.addListener("keyboardDidHide", () =>
+      setKeyboardShown(false)
+    );
+
+    return () => {
+      showSubscription.remove();
+      hideSubscription.remove();
+    };
+  }, []);
 
   const windowHeight = useWindowDimensions().height;
   const onLayout = (ev: LayoutChangeEvent) => {
     if (ev.nativeEvent.layout.height > windowHeight) {
-      setScrollEnabled(true);
+      setviewOverflows(true);
     }
   };
 
