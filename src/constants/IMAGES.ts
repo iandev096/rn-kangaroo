bcf9791443f02907fafe7cd878a8472f3b34721c
Isaac Anyimiah <iandev096@gmail.com>
2022-09-06 11:27:21 +0000

    added icons for DrawerScreenHeader - MENU_ICON - KANGAROO_ICON

--- src/constants/IMAGES.ts
+++ src/constants/IMAGES.ts
@@ -1,4 +1,6 @@
 export default {
   KANGAROO_LOGO: require("../../assets/images/kangaroo-logo.png"),
   AUTH_HEADER_BANNER: require("../../assets/images/auth-header-banner.png"),
+  MENU_ICON: require("../../assets/icons/menu-icon.png"),
+  KANGAROO_ICON: require("../../assets/icons/kangaroo-icon.png"),
 };
