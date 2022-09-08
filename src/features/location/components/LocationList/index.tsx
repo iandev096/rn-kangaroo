4c2d9d99b7982334d04e4d04b37975a459c1f32a
Isaac Anyimiah <iandev096@gmail.com>
2022-09-08 09:58:38 +0000

    LocationList WIP

--- src/features/location/components/LocationList/index.tsx
+++ src/features/location/components/LocationList/index.tsx
@@ -1,14 +1,52 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Animated, StyleSheet, Text, View } from "react-native";
+import BottomAction from "src/components/BottomAction";
+import SkeletonBlock from "src/components/SkeletonBlock";
+import COLOR from "src/constants/COLOR";
+import { AnimatedViewStyle } from "src/types/animated";
+import LocationListSkeleton from "./LocationListSkeleton";
 
-type Props = {};
+type Props = {
+  pageRevealValue: Animated.Value;
+};
 
-function LocationList({}: Props) {
+function LocationList({ pageRevealValue }: Props) {
+  const animStyle: AnimatedViewStyle = {
+    transform: [
+      {
+        translateY: pageRevealValue.interpolate({
+          inputRange: [0, 1],
+          outputRange: [400, 0],
+        }),
+      },
+      {
+        scale: pageRevealValue.interpolate({
+          inputRange: [0, 1],
+          outputRange: [0.6, 1],
+        }),
+      },
+    ],
+    opacity: pageRevealValue,
+  };
+
   return (
-    <View>
-      <Text>LocationList</Text>
-    </View>
+    <Animated.View style={[styles.cardContainer, animStyle]}>
+      {/* replace with flatList */}
+      <View>
+        <Text>LocationList</Text>
+        <LocationListSkeleton />
+      </View>
+      <BottomAction />
+    </Animated.View>
   );
 }
+
+const styles = StyleSheet.create({
+  cardContainer: {
+    marginTop: -60,
+    backgroundColor: COLOR.PRIMARY_B,
+    flexGrow: 1,
+  },
+});
 
 export default LocationList;
