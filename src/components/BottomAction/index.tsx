70ec81f7cb006cfb725c0a6135d64e73716657f0
Isaac Anyimiah <iandev096@gmail.com>
2022-09-13 10:14:07 +0000

    BottomAction component WIP - TODO: make component configurable

--- src/components/BottomAction/index.tsx
+++ src/components/BottomAction/index.tsx
@@ -1,14 +1,45 @@
 import React from "react";
-import { Text, View } from "react-native";
+import {
+  Dimensions,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { FONT_FAMILY } from "src/constants/FONT";
 
 type Props = {};
 
 function BottomAction({}: Props) {
   return (
-    <View>
-      <Text>BottomAction</Text>
-    </View>
+    <TouchableOpacity activeOpacity={0.8} style={styles.container}>
+      <View style={styles.content}>
+        <Ionicons name="map-sharp" size={17} color="#AEAEAE" />
+        <Text style={styles.text}>Choose location on map</Text>
+      </View>
+    </TouchableOpacity>
   );
 }
+
+export const styles = StyleSheet.create({
+  container: {
+    width: Dimensions.get("screen").width,
+    height: 47,
+    alignItems: "center",
+  },
+  content: {
+    flexDirection: "row",
+    alignItems: "center",
+    flex: 1,
+  },
+  text: {
+    marginLeft: 16,
+    fontFamily: FONT_FAMILY.BR_FIRMA_MEDIUM,
+    lineHeight: 20,
+    fontSize: 16,
+  },
+});
 
 export default BottomAction;
