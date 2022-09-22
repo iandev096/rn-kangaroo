640049b6306da321b74ba6a8f18d6605c697e251
Isaac Anyimiah <iandev096@gmail.com>
2022-09-22 11:19:53 +0000

    added current position hook - handles permissions needed for location - gets current position of the user

---
+++ src/hooks/useCurrentPosition.ts
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import * as Location from "expo-location";
+
+function useCurrentPosition(deps: any[] = []) {
+  const [location, setLocation] = useState<Location.LocationObject>();
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    let isMounted = true;
+    async function initialize() {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+
+      if (status !== "granted" && isMounted) {
+        setErrorMessage("Permission to access location was not granted");
+        setLocation(undefined);
+        return;
+      }
+
+      const location = await Location.getCurrentPositionAsync();
+
+      if (isMounted) {
+        setLocation(location);
+        setErrorMessage("");
+      }
+    }
+
+    initialize();
+
+    () => (isMounted = false);
+  }, [...deps]);
+
+  return { location, errorMessage };
+}
+
+export default useCurrentPosition;
