d972ba6e9667999804dd3e64c39940779634cb16
Isaac Anyimiah <iandev096@gmail.com>
2022-09-22 11:17:40 +0000

    added image assets - CUR_LOC - MAP_BACK

--- src/constants/IMAGES.ts
+++ src/constants/IMAGES.ts
@@ -4,4 +4,6 @@ export default {
   MENU_ICON: require("../../assets/icons/menu-icon.png"),
   KANGAROO_ICON: require("../../assets/icons/kangaroo-icon.png"),
   LOCATION_HEADER_BG: require("../../assets/images/location-header-bg.png"),
+  MAP_BACK: require("../../assets/icons/map-back.png"),
+  CUR_LOC: require("../../assets/icons/cur-loc.png"),
 };
