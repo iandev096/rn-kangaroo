aa69ce6dd97dc0ef36c6ab1215b14143b18eee4d
Isaac Anyimiah <iandev096@gmail.com>
2022-09-26 10:10:46 +0000

    modularized BackButton

---
+++ src/components/BackButton/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Pressable, PressableProps, StyleSheet } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import COLOR from "src/constants/COLOR";
+
+type Props = {} & PressableProps;
+
+function BackButton({ onPress, hitSlop = 8 }: Props) {
+  return (
+    <Pressable onPress={onPress} hitSlop={hitSlop} style={styles.backBtn}>
+      <Ionicons name="arrow-back" size={23} color={COLOR.PRIMARY_A} />
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  backBtn: {
+    width: 32,
+    marginRight: 16,
+    alignItems: "center",
+    zIndex: 110,
+  },
+});
+
+export default BackButton;
