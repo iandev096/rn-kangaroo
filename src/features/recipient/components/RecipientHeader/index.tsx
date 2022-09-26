68989390156a0ba64283a59b1a44035283aee662
Isaac Anyimiah <iandev096@gmail.com>
2022-09-26 10:19:54 +0000

    added RecipientHeader

---
+++ src/features/recipient/components/RecipientHeader/index.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import TransHeader from "src/components/TransHeader";
+import COLOR from "src/constants/COLOR";
+import { FONT_FAMILY } from "src/constants/FONT";
+
+type Props = {};
+
+function RecipientHeader({}: Props) {
+  return (
+    <View style={styles.header}>
+      <TransHeader
+        onPressBack={() => console.log("pressed")}
+        bottom={<Text style={styles.title}>Recipient</Text>}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    paddingHorizontal: 20,
+    marginTop: 16,
+  },
+  title: {
+    fontSize: 48,
+    lineHeight: 61,
+    fontFamily: FONT_FAMILY.BR_FIRMA_MEDIUM,
+    color: COLOR.PRIMARY_A,
+  },
+});
+
+export default RecipientHeader;
