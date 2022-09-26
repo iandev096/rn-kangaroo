d22204838064494255209503158dfdf0a1f64c2d
Isaac Anyimiah <iandev096@gmail.com>
2022-09-26 10:20:32 +0000

    added Drawer to HomeScreen

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,30 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Button from "src/components/Button";
-import { DrawerScreenHeader } from "src/features/drawer";
+import { Drawer, DrawerScreenHeader } from "src/features/drawer";
 import { DeliverToForm } from "src/features/location";
 import { AppStackProps } from "src/features/navigation/components/AppStack/types";
 
 type Props = {} & AppStackProps;
 
 function HomeScreen({ navigation }: Props) {
+  const [showDrawer, setShowDrawer] = useState(false);
+
   return (
-    <SafeAreaView style={styles.screen}>
-      <DrawerScreenHeader
-        onMenuPress={() => console.log("pressed handburger")}
-      />
+    <>
+      <SafeAreaView style={styles.screen}>
+        <DrawerScreenHeader onMenuPress={() => setShowDrawer(true)} />
 
-      <View>
-        <DeliverToForm
-          onPressTextField={() => navigation.navigate("EnterLocation")}
-        />
-      </View>
+        <View>
+          <DeliverToForm
+            onPressTextField={() => navigation.navigate("EnterLocation")}
+          />
+        </View>
 
-      <View style={styles.buttonContainer}>
-        <Button title="Continue" />
-      </View>
-    </SafeAreaView>
+        <View style={styles.buttonContainer}>
+          <Button title="Continue" />
+        </View>
+      </SafeAreaView>
+      <Drawer onClose={() => setShowDrawer(false)} show={showDrawer} />
+    </>
   );
 }
 
