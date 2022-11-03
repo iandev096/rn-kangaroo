f29dc49b6c0afa1c5211b62c942eeef2c2786108
Isaac Anyimiah <iandev096@gmail.com>
2022-11-03 08:40:41 +0000

    added variant

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
-import { FONT_FAMILY } from "src/constants/FONT";
 import COLOR from "src/constants/COLOR";
+import { FONT_FAMILY } from "src/constants/FONT";
 
-type Props = { title: string } & TouchableOpacity["props"];
+type Variant = "normal" | "alternate";
+type Props = {
+  title: string;
+  variant?: Variant;
+} & TouchableOpacity["props"];
 
-function Button({ title, style = {}, ...props }: Props) {
+function Button({ title, style = {}, variant = "normal", ...props }: Props) {
+  const styles = getStyles(variant);
+
   return (
     <TouchableOpacity
       activeOpacity={0.8}
@@ -22,20 +28,31 @@ function Button({ title, style = {}, ...props }: Props) {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: COLOR.PRIMARY_A,
-    paddingHorizontal: 20,
-    paddingVertical: 16,
-    borderRadius: 3,
-    alignItems: "center",
-  },
-  text: {
-    fontSize: 18,
-    fontFamily: FONT_FAMILY.BR_FIRMA_MEDIUM,
-    lineHeight: 24,
-    color: COLOR.PRIMARY_B,
-  },
-});
+const backgroundColor: Record<Variant, string> = {
+  alternate: COLOR.GRAY_100,
+  normal: COLOR.PRIMARY_A,
+};
+
+const color: Record<Variant, string> = {
+  normal: COLOR.PRIMARY_B,
+  alternate: COLOR.PRIMARY_A,
+};
+
+const getStyles = (variant: Variant) =>
+  StyleSheet.create({
+    container: {
+      backgroundColor: backgroundColor[variant],
+      paddingHorizontal: 20,
+      paddingVertical: 16,
+      borderRadius: 3,
+      alignItems: "center",
+    },
+    text: {
+      fontSize: 18,
+      fontFamily: FONT_FAMILY.BR_FIRMA_MEDIUM,
+      lineHeight: 24,
+      color: color[variant],
+    },
+  });
 
 export default Button;
