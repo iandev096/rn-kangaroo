1634b9f067a8150044ae43e819791516924ade8e
Isaac Anyimiah <iandev096@gmail.com>
2022-11-07 00:12:05 +0000

    refactored error handling logic

--- src/hooks/useCurrentPosition.ts
+++ src/hooks/useCurrentPosition.ts
@@ -1,21 +1,25 @@
 import * as Location from "expo-location";
 import { useCallback, useEffect, useState } from "react";
 
-function useCurrentPosition(deps: any[] = [], onError: (msg: string) => any) {
+function defaultOnError(msg: string){
+  console.log(msg)
+}
+
+function useCurrentPosition(deps: any[] = [], onError = defaultOnError) {
   const [location, setLocation] = useState<Location.LocationObject>();
 
   const initLocation = useCallback(async () => {
     let isMounted = true;
 
     try {
-      const hasServieEnabled = await Location.hasServicesEnabledAsync();
-      if (!hasServieEnabled) {
-        const { status } = await Location.requestForegroundPermissionsAsync();
+      if (!(await Location.hasServicesEnabledAsync())) return onError('You have not enabled location services.');
 
-        if (status !== "granted" && isMounted) {
-          onError("Permission to access location was not granted");
-          setLocation(undefined);
-        }
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      
+      if (status !== "granted" && isMounted) {
+        onError("Permission to access location was not granted");
+        setLocation(undefined);
+        return;
       }
 
       const location = await Location.getCurrentPositionAsync();
@@ -24,7 +28,8 @@ function useCurrentPosition(deps: any[] = [], onError: (msg: string) => any) {
         setLocation(location);
       }
     } catch (err: any) {
-      onError(err?.message ?? '' )
+      console.log(err)
+      onError('Could not fetch current location');
     }
 
     return () => {
