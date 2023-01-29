3c5a3d4f65a737af171871c3d6c5218d7d209aad
Isaac Anyimiah <iandev096@gmail.com>
2023-01-29 22:17:19 +0000

    added feature unit - HistoryDetailHeader

---
+++ src/features/history/components/HistoryDetailHeader/index.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import BackButton from "src/components/BackButton";
+import TransHeader from "src/components/TransHeader";
+import COLOR from "src/constants/COLOR";
+import { FONT_FAMILY } from "src/constants/FONT";
+
+type Props = {
+  title: string;
+};
+
+function HistoryDetailHeader({ title }: Props) {
+  return (
+    <View style={styles.header}>
+      <TransHeader
+        left={<BackButton onPress={() => console.log("pressed")} />}
+        bottom={<Text style={styles.title}>{title}</Text>}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    paddingHorizontal: 16,
+    marginTop: 16,
+    marginBottom: 8,
+  },
+  title: {
+    fontSize: 32,
+    lineHeight: 32,
+    fontFamily: FONT_FAMILY.BR_FIRMA_MEDIUM,
+    color: COLOR.PRIMARY_A,
+    marginTop: 16,
+  },
+});
+
+export default HistoryDetailHeader;
