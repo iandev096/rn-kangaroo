9513d77e2df928e07e41c93482824af13038ab0c
Isaac Anyimiah <iandev096@gmail.com>
2023-01-29 22:23:02 +0000

    added reporting-problem feature - ReportProblemHeader - ReportProblemDetail - CommonIssuesBottomSheet - CustomIssuesBottomSheet - SubmitProgressModal - SubmitProgress (Type)

---
+++ src/features/reporting-problem/index.ts
@@ -0,0 +1,16 @@
+import CommonIssuesBottomSheet from "./components/CommonIssuesBottomSheet";
+import CustomIssuesBottomSheet from "./components/CustomIssuesBottomSheet";
+import ReportProblemDetail from "./components/ReportProblemDetail";
+import ReportProblemHeader from "./components/ReportProblemHeader";
+import SubmitProgressModal, {
+  SubmitProgress,
+} from "./components/SubmitProgressModal";
+
+export {
+  ReportProblemHeader,
+  ReportProblemDetail,
+  CommonIssuesBottomSheet,
+  CustomIssuesBottomSheet,
+  SubmitProgressModal,
+  SubmitProgress,
+};
