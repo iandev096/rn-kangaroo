d3dbf1360b9f655dbda55a4e0435876f65ba5b6b
Isaac Anyimiah <iandev096@gmail.com>
2023-01-29 22:10:40 +0000

    added to History feature - HistoryDetail - HistoryDetailHeader - HistoryDriverProfileBottomSheet - HistoryRecipientContactBottomSheet

--- src/features/history/index.tsx
+++ src/features/history/index.tsx
@@ -1,5 +1,17 @@
+import HistoryDetail from "./components/HistoryDetail";
+import HistoryDetailHeader from "./components/HistoryDetailHeader";
+import HistoryDriverProfileBottomSheet from "./components/HistoryDriverProfileBottomSheet";
 import HistoryFilterBottomSheet from "./components/HistoryFilterBottomSheet";
 import HistoryList from "./components/HistoryList";
 import HistoryListHeader from "./components/HistoryListHeader";
+import HistoryRecipientContactBottomSheet from "./components/HistoryRecipientContactBottomSheet";
 
-export { HistoryList, HistoryFilterBottomSheet, HistoryListHeader };
+export {
+  HistoryList,
+  HistoryFilterBottomSheet,
+  HistoryListHeader,
+  HistoryDetail,
+  HistoryDetailHeader,
+  HistoryDriverProfileBottomSheet,
+  HistoryRecipientContactBottomSheet,
+};
