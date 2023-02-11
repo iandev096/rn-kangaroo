c99b6ad0a6ba053a72d9df619d744be5cdcc24e1
Isaac Anyimiah <iandev096@gmail.com>
2023-02-11 11:37:49 +0000

    exposed settings feature

---
+++ src/features/settings/index.ts
@@ -0,0 +1,7 @@
+import ProfileForm from "./components/ProfileForm";
+import ProfileHeader from "./components/ProfileHeader";
+import SettingsHeader from "./components/SettingsHeader";
+import SettingsList from "./components/SettingsList";
+import UserInfo from "./components/UserInfo";
+
+export { SettingsHeader, UserInfo, SettingsList, ProfileHeader, ProfileForm };
