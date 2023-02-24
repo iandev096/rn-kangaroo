a7c67fdbb01438dadbb3d111d5ac3b98cb5a5b79
Isaac Anyimiah <iandev096@gmail.com>
2023-02-24 09:32:35 +0000

    exposed Miscallaneous components

---
+++ src/features/miscellaneous/index.ts
@@ -0,0 +1,21 @@
+import AboutHeader from "./components/AboutHeader";
+import AboutInfo from "./components/AboutInfo";
+import AboutList from "./components/AboutList";
+import ContactUsDetail from "./components/ContactUsDetail";
+import ContactUsHeader from "./components/ContactUsHeader";
+import FaqAnswerDetail from "./components/FaqAnswerDetail";
+import FaqCategoriesList from "./components/FaqCategoriesList";
+import FaqHeader from "./components/FaqHeader";
+import FaqQuestionsList from "./components/FaqQuestionsList";
+
+export {
+  FaqHeader,
+  FaqCategoriesList,
+  FaqQuestionsList,
+  FaqAnswerDetail,
+  ContactUsHeader,
+  ContactUsDetail,
+  AboutHeader,
+  AboutInfo,
+  AboutList,
+};
