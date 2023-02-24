33eaa3159d9e13d48d679d2057490f03ecdf8d07
Isaac Anyimiah <iandev096@gmail.com>
2023-02-24 09:36:11 +0000

    added notification feature and exposed components

---
+++ src/features/notification/index.ts
@@ -0,0 +1,9 @@
+import NotificationDetailBottomSheet from "./components/NotificationDetailBottomSheet";
+import NotificationsHeader from "./components/NotificationsHeader";
+import NotificationsSectionList from "./components/NotificationsSectionList";
+
+export {
+  NotificationsHeader,
+  NotificationsSectionList,
+  NotificationDetailBottomSheet,
+};
